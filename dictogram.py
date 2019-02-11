25450253a1edbe81988b17c512dfd1471ed2869b
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-02-10 16:57:55 -0800

    Finished add_count and frequency methods of dictogram class

--- dictogram.py
+++ dictogram.py
@@ -20,10 +20,21 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if word not in self:
+            self.types += 1
+            self.tokens += count
+            self[word] = count
+        else:
+            self.tokens += count
+            self[word] += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        if word in self:
+            return self[word]
+        else:
+            return 0
 
 
 def print_histogram(word_list):
