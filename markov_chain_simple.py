2b3010d72fc1304bb1a1c67e6af7788fe13fb981
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-02-17 16:16:25 -0800

    Added logic to get next word for each element in text, updating markov chain still WIP

--- markov_chain_simple.py
+++ markov_chain_simple.py
@@ -2,19 +2,19 @@ old_text = "one fish two fish red fish blue fish"
 
 # Convert sample to array (more readable)
 text = old_text.split(' ')
-text.append("STOP")
+text.append("*STOP*")
 
-histogram = {}
+markov_chain = {}
 
 
 def update_histogram(word):
     word = word.lower()
     word_found = False
-    for key in histogram.keys():
+    for key in markov_chain.keys():
         if key == word:
             break
     if word_found == False:
-        histogram.update({word: {}})
+        markov_chain.update({word: {}})
 
 
 def init_histogram():
@@ -24,16 +24,27 @@ def init_histogram():
 
 
 def update_chain():
-    count = 0
+    count = 0  # Adding count for relative position (more reliable than index)
     for item in text:
-        if item != "STOP":
-            position = count
+        if item != "*STOP*":  # Checking for hard stop token
+
+            position = count  # Using relative position
             count += 1
+            # Getting next word for each item
             next_word = text[(int(position) + 1)]
-        print(str(item) + str(position) + str(next_word))
+            print(item + next_word)
+            # TODO: Update Markov dictionary
+            for key in markov_chain[item].keys():
+                if key != "*STOP*":
+                    word_found = False
+                    if key == next_word:
+                        word_found = True
+                        histogram[item][key] += 1
+                    if word_found == False:
+                        markov_chain[item].update({next_word: 1})
+    print(markov_chain)
 
 
 init_histogram()
-print(histogram)
 
 update_chain()
